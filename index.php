6675979fecfd19b4887faff03eb07d64d0df8b0b
JustinRaz <justinrazmont@gmail.com>
2020-06-28 20:23:52 +0800

    basic stuff

--- index.php
+++ index.php
@@ -19,19 +19,12 @@ if(!$connection){
 <html lang="en">
 <head>
     <title>AdminForPhp</title>
-
-    <style>
-    .container{
-        display: flex;
-    }
-    .fixed{
-        width: 220px;
-    }
-    .flex-item{
-        flex-grow: 1;
-    }
-    
-    </style>
+    <link rel="stylesheet" href="style.css">
+    <script 
+        src="https://code.jquery.com/jquery-3.4.1.min.js" 
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" 
+        crossorigin="anonymous">
+    </script>
 </head> 
 <body>
     <h1>
@@ -40,28 +33,44 @@ if(!$connection){
 
     <div class="container">
 
-        <div id="db_list" class="fixed">
-            <h3>Databases</h3>
-            <?php
-            $query_db_list = "SHOW DATABASES";
-            $results_db_list = $connection->query($query_db_list);
-            ?>
+        <div id="db_list_container" class="fixed">
+            <div id="db_list_title_container">
+                <h3 id="db_list_title"
+                <?php if(isset($_GET["db"])):?>
+                        style="transform: rotate(90deg)"
+                    <?php else:?>
+                        
+                    <?php endif?>
+                >Databases</h3>
 
-            <?php if ($results_db_list->num_rows > 0) : ?>
-                <?php while($row = $results_db_list->fetch_assoc()): ?>
-                    <li> <a class='nav-link active' href='?db=<?php echo $row["Database"]?>'><?php echo $row["Database"]?> </a></li>
-                    <?php endwhile?>
-            <?php endif?> 
+
+            </div>
+
+
+            <ul id="db_list" 
+                <?php if(isset($_GET["db"])):?>
+                    style="display: none"
+                <?php endif?>
+            >
+                <?php
+                $query_db_list = "SHOW DATABASES";
+                $results_db_list = $connection->query($query_db_list);
+                ?>
+                <?php if ($results_db_list->num_rows > 0) : ?>
+                    <?php while($row = $results_db_list->fetch_assoc()): ?>
+                        <li> <a class='nav-link active' href='?db=<?php echo $row["Database"]?>'><?php echo $row["Database"]?> </a></li>
+                        <?php endwhile?>
+                <?php endif?> 
+            </ul>
         </div>
 
         <div id="db_body" class="flex-item">
             <?php if(isset($_GET["db"])):?>
                 <h2><?php echo $_GET["db"]?></h2>
                 <div id="crud_list">
-                    <li>Create</li>
-                    <li>Retrieve</li>
-                    <li>Update</li>
-                    <li>Delete</li>
+                <ul>
+                    <li>Select</li>
+                </ul>
                 
                 </div>
             <?php endif?>
@@ -70,4 +79,5 @@ if(!$connection){
 
     </div>
 </body>
+<script src="main.js"></script>
 </html>
