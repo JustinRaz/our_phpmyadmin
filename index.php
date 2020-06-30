77a398383b015914a359bdddc0bcb87354937295
JustinRaz <justinrazmont@gmail.com>
2020-06-30 11:01:06 +0800

    added: +db and +table forms

--- index.php
+++ index.php
@@ -6,7 +6,15 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
-$connection = mysqli_connect($servername, $username, $password);
+// $_GET[] VARIABLES ESTABLISHED
+// 1. db - indicates current db
+// 2. table - indicates current db
+
+if(isset($_GET["db"])){
+    $connection = mysqli_connect($servername, $username, $password, $_GET["db"]);
+}else{
+    $connection = mysqli_connect($servername, $username, $password);
+}
 
 if(!$connection){
     die("Connection failed: ".$connection->connect_error);
@@ -49,6 +57,13 @@ if(!$connection){
                 $query_db_list = "SHOW DATABASES";
                 $results_db_list = $connection->query($query_db_list);
                 ?>
+                <li>
+                <!-- FORM 1 -->
+                    <form action="" method="post">
+                        <input type="text" name="" placeholder="Add database">
+                        <input type="submit" value="+">
+                    </form>
+                </li>
                 <?php if ($results_db_list->num_rows > 0) : ?>
                     <?php while($row = $results_db_list->fetch_assoc()): ?>
                         <li> <a class='nav-link active' href='?db=<?php echo $row["Database"]?>'><?php echo $row["Database"]?> </a></li>
@@ -57,18 +72,46 @@ if(!$connection){
             </ul>
         </div>
 
-        <div id="db_body" class="flex-item">
-            <?php if(isset($_GET["db"])):?>
-                <h2><?php echo $_GET["db"]?></h2>
-                <div id="crud_list">
-                <ul>
-                    <li>Select</li>
-                </ul>
-                
+        <?php if(isset($_GET["db"])):?>
+            <div id="db_body" class="flex-item">
+                <div id="db_tables">
+                    <?php
+                    $query_db_table_list = "SHOW tables";
+                    $results_db_table_list = $connection->query($query_db_table_list);
+                    ?>
+                    <?php if($results_db_table_list->num_rows==0):?>
+                        <h2>Database is empty.</h2>
+                    <?php else:?>
+                        <h2>Tables</h2>
+                    <?php endif?>
+                    <ul id="db_table_list">
+                        <li>
+                        <!-- FORM 2 -->
+                            <form action="" method="post">
+                                <input type="text" name="" placeholder="Add table">
+                                <input type="submit" value="+">
+                            </form>
+                        </li>
+                        <?php if ($results_db_table_list->num_rows > 0): ?>
+                            <?php while($row = $results_db_table_list->fetch_assoc()): ?>
+                                <li><a class='nav-link active' href='?db=<?php echo $row["Tables_in_".$_GET['db']]?>'><?php echo $row["Tables_in_".$_GET['db']]?> </a></li>
+                            <?php endwhile?>
+                        <?php endif?> 
+                    </ul>
                 </div>
-            <?php endif?>
 
-        </div>
+                <div id="db_operations">
+                        <h2><?php echo $_GET["db"]?></h2>
+                        <div id="crud_list">
+                        <ul>
+                            <li>Select</li>
+                        </ul>
+                        
+                        </div>
+                </div>
+
+            </div>
+        <?php endif?>
 
     </div>
 </body>
